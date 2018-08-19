b9022751cffc95e81a0b8e45d164598ad6336f7a
Wataru Nakata <wataru9871@gmail.com>
2018-08-20 09:15:11 +1000

    recieving worked

---
+++ tcpclientClass.py
@@ -0,0 +1,19 @@
+import socket
+
+class tcpClient(socket.socket):
+    """docstring for tcpClient."""
+    def __init__(self):
+        self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def setHost(self,host,port):
+        self._host = host
+        self._port = port
+
+    def connect(self):
+        self._client.connect((self._host,self._port))
+
+    def send(self,msg):
+        self._client.send(msg)
+
+    def _recieve(self):
+        return self._client.recv(4096)
