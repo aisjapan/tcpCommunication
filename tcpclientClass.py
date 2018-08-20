b8e4e2f8d87a91340afd1036439350a949f71929
Wataru Nakata <wataru9871@gmail.com>
2018-08-21 09:47:03 +1000

    private functions added

--- tcpclientClass.py
+++ tcpclientClass.py
@@ -3,17 +3,17 @@ import socket
 class tcpClient(socket.socket):
     """docstring for tcpClient."""
     def __init__(self):
-        self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def setHost(self,host,port):
-        self._host = host
-        self._port = port
+        self.__host = host
+        self.__port = port
 
     def connect(self):
-        self._client.connect((self._host,self._port))
+        self.__client.connect((self.__host,self.__port))
 
     def send(self,msg):
-        self._client.send(msg)
+        self.__client.send(msg)
 
     def recieve(self):
-        return self._client.recv(4096)
+        return self.__client.recv(4096)
--- tcpserverClass.py
+++ tcpserverClass.py
@@ -3,50 +3,50 @@ import threading
 
 class tcpserver(socket.socket, threading.Thread):
     def __init__(self):
-        self._host = ""
-        self._port = None
-        self._serversock = None
-        self._maxClients = 0
-        self._recievedMsg = []
-        self._clientSock = []
-        self._serverThread = None
+        self.__host = ""
+        self.__port = None
+        self.__serversock = None
+        self.__maxClients = 0
+        self.__recievedMsg = []
+        self.__clientSock = []
+        self.__serverThread = None
 
     def setServer(self,host,port,maxClients):
-        self._host = host
-        self._port = port
-        self._maxClients = maxClients
+        self.__host = host
+        self.__port = port
+        self.__maxClients = maxClients
 
     def __server(self):
         for i in range(self._maxClients):
-            self._clientSock.append(self._serversock.accept())
-            print(self._clientSock[i][1])
+            self.__clientSock.append(self.__serversock.accept())
+            print(self.__clientSock[i][1])
 
     def sendAll(self,msg):
-        for e in self._clientSock:
+        for e in self.__clientSock:
             e[0].sendall(msg.encode('utf-8'))
 
     def __recieve(self):
         while True:
-            for e in self._clientSock:
+            for e in self.__clientSock:
                 rcvmsg = e[0].recv(4096)
-                self._recievedMsg.append(rcvmsg.decode('utf-8'))
+                self.__recievedMsg.append(rcvmsg.decode('utf-8'))
 
     def getMsg(self):
-        if len(self._recievedMsg) != 0:
-            msg = self._recievedMsg[0]
-            self._recievedMsg.pop(0)
+        if len(self.__recievedMsg) != 0:
+            msg = self.__recievedMsg[0]
+            self.__recievedMsg.pop(0)
             return True,msg
         else:
             return False, ''
 
     def startServer(self):
-        self._serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._serversock.bind((self._host,self._port)) #IPとPORTを指定してバインドします
-        self._serversock.listen(10) #接続の待ち受けをします（キューの最大数を指定）
-        self._serverThread = threading.Thread(target=self._server, name="server",args=())
-        self._recieveThread = threading.Thread(target=self._recieve, name= "recieve",args=())
-        self._serverThread.setDaemon(True)
-        self._recieveThread.setDaemon(True)
-        self._serverThread.start()
-        self._recieveThread.start()
+        self.__serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.__serversock.bind((self.__host,self.__port)) #IPとPORTを指定してバインドします
+        self.__serversock.listen(10) #接続の待ち受けをします（キューの最大数を指定）
+        self.__serverThread = threading.Thread(target=self.__server, name="server",args=())
+        self.__recieveThread = threading.Thread(target=self.__recieve, name= "recieve",args=())
+        self.__serverThread.setDaemon(True)
+        self.__recieveThread.setDaemon(True)
+        self.__serverThread.start()
+        self.__recieveThread.start()
