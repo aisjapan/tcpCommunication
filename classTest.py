1d63e49bff6530cbdf0a8df2690414939de2dd76
HIROKI <hiroki4002@gmail.com>
2018-08-23 10:28:31 +1000

    send json
    sendJson2Dueを呼ぶことでjsonを送る
    MessageTypeの値が1の時，動くように高野に設定してもらう

--- classTest.py
+++ classTest.py
@@ -3,6 +3,34 @@ import socket
 import threading
 import time
 import aisprotocol
+import serial
+
+
+#--------------追記------------------
+#Json
+f = open('RaspberryPiMessage_A.json', 'r')
+send_message_dict = json.load(f)
+
+#Serial
+port = "/dev/ttyS0"
+serialFromArduino = serial.Serial(port, 115200)
+serialFromArduino.flushInput()
+
+
+#Serial Communication
+def SendJson2Due():
+  # MessageTypeはデフォルトで0　そして1で動くように高野にArduino側のプログラムを変更してもらう
+  send_message_dict["MessageType"] = 1
+
+  send_str = json.dumps(send_message_dict)
+  send_str = send_str + '\n'
+  serialFromArduino.write(bytes(send_str.encode("utf-8")))
+  print(send_str)
+  t=threading.Timer(1,SendJson2Due)
+  t.start()
+
+#------------------------------------
+
 
 server = tcpserverClass.tcpserver()
 
@@ -24,3 +52,7 @@ while True:
         if count == 3:
             server.sendAll("START")
             
+            t=threading.Thread(target=SendJson2Due)
+            t.start()
+
+serialFromArduino.close()
